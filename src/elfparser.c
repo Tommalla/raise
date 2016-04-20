509b3815de87b07ab2108654b7dbe326a11f0711
Tomasz Zakrzewski <t.zakrzewski@student.uw.edu.pl>
2016-04-20 23:54:20 +0200

    A small refactor and fixing small issues.

--- src/elfparser.c
+++ src/elfparser.c
@@ -13,6 +13,37 @@
 char desc_buf[100000];
 void *mapped_mem[100000];
 
+void process_note_file(const char* buffer) {
+	// This helper method assumes that 
+	long count, page_size;
+	const void *ptr = buffer;
+	memcpy(&count, ptr, sizeof(long));
+	ptr += sizeof(long);
+	memcpy(&page_size, ptr, sizeof(long));
+	ptr += sizeof(long);
+	long off = 0;
+	const char *sptr = ptr + 3 * sizeof(long) * count;
+	for (off = 0; off < count; ++off) {
+		unsigned long start, end, page_of;
+		long offset = off * 3 * sizeof(long);
+		memcpy(&start, ptr + offset, sizeof(long));
+		memcpy(&end, ptr + offset + sizeof(long), sizeof(long));
+		memcpy(&page_of, ptr + offset + 2 * sizeof(long), sizeof(long));
+		int map_fd = open(sptr, O_RDONLY);
+		// move to next NUL
+		printf("%#08x %#08x %#08x %s\n", start, end, page_of, sptr); 
+		for(; *sptr != '\0'; ++sptr);
+		sptr++;
+		
+		// FINALLY map the memory
+		// TODO RWE
+		mmap((void *)start, end - start, PROT_EXEC | PROT_READ | PROT_WRITE, MAP_PRIVATE, map_fd, page_of);
+		
+		//mmap(NULL
+		close(map_fd);
+	}
+}
+
 void process_elf(char *path, Elf32_Ehdr *hdr, Elf32_Phdr *phdrs) {
 	int i;
 	FILE *f = fopen(path, "r");
@@ -34,7 +65,7 @@ void process_elf(char *path, Elf32_Ehdr *hdr, Elf32_Phdr *phdrs) {
 		printf("Read the program segment header! %#08X\n", phdrs[i].p_type);
 		// FIXME: RWE?
 		mapped_mem[i] = mmap((void *)phdrs[i].p_vaddr, phdrs[i].p_memsz, PROT_EXEC | PROT_READ | PROT_WRITE, 
-				     MAP_PRIVATE | MAP_ANONYMOUS, fd, phdrs[i].p_offset);
+				     MAP_PRIVATE | MAP_ANONYMOUS, -1, phdrs[i].p_offset);
 		if (mapped_mem[i] < 0) {
 			printf("\tError! Couldn't map the anonymous memory area!");
 		}
@@ -47,52 +78,43 @@ void process_elf(char *path, Elf32_Ehdr *hdr, Elf32_Phdr *phdrs) {
 			fseek(f, phdrs[i].p_offset, SEEK_SET);
 			size_t read = 0;
 			while (read < phdrs[i].p_filesz) {
+				// Read the note header
 				Elf32_Nhdr nhdr;
 				fread(&nhdr, sizeof(Elf32_Nhdr), 1, f);
+				read += sizeof(Elf32_Nhdr);
+				
+				// Skip name (we don't use it anyway)
 				int padding = (4 - (nhdr.n_namesz % 4)) % 4;
-				read += sizeof(Elf32_Nhdr) + padding;
-				printf("\t(%d) Read successful: type = %#08X, descsz = %d\n", read, nhdr.n_type, nhdr.n_descsz);
 				fseek(f, nhdr.n_namesz + padding, SEEK_CUR);
-				fread(desc_buf, nhdr.n_descsz, 1, f);
 				read += nhdr.n_namesz + padding;
+				
+				// Read the description.
+				fread(desc_buf, nhdr.n_descsz, 1, f);
 				padding = (4 - (nhdr.n_descsz % 4)) % 4;
 				read += nhdr.n_descsz + padding;
+				
+				// Skip padding
 				fseek(f, padding, SEEK_CUR);
+				
+				printf("\t(%d) Note header read successfully: type = %#08X, descsz = %d\n", read, 
+				       nhdr.n_type, nhdr.n_descsz);
 				// FIXME NT_PRSTATUS
-				if (nhdr.n_type != NT_FILE) {
-					puts("\tNot an NT_FILE, skipping");
-					continue;
-				} else {
-					long count, page_size;
-					void *ptr = desc_buf;
-					memcpy(&count, ptr, sizeof(long));
-					ptr += sizeof(long);
-					memcpy(&page_size, ptr, sizeof(long));
-					ptr += sizeof(long);
-					long off = 0;
-					char *sptr = ptr + 3 * sizeof(long) * count;
-					for (off = 0; off < count; ++off) {
-						long start, end, page_of;
-						long offset = off * 3 * sizeof(long);
-						memcpy(&start, ptr + offset, sizeof(long));
-						memcpy(&end, ptr + offset + sizeof(long), sizeof(long));
-						memcpy(&page_of, ptr + offset + 2 * sizeof(long), sizeof(long));
-						int map_fd = open(sptr, O_RDONLY);
-						// move to next NUL
-						printf("%#08X %#08X %#08X %s\n", start, end, page_of, sptr); 
-						for(; *sptr != '\0'; ++sptr);
-						sptr++;
-						
-						// FINALLY map the memory
-						// TODO RWE
-						mmap((void *)start, end - start, PROT_EXEC | PROT_READ | PROT_WRITE, 
-						     MAP_PRIVATE, map_fd, page_of);
-						
-						//mmap(NULL
-						close(map_fd);
-					}
-					
-				}
+				switch (nhdr.n_type) {
+					case NT_FILE:
+						puts("\tNT_FILE");
+						process_note_file(desc_buf);
+						break;
+					case NT_PRSTATUS:
+						puts("\tNT_PRSTATUS");
+						// TODO
+						break;
+					case NT_386_TLS:
+						puts("\tNT_386_TLS");
+						break;
+					default:
+						puts("\tNot implemented note type. Ignoring");
+						break;
+				};
 			}
 		}
 	}
